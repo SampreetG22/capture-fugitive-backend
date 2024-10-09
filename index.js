347314e637908f1af2e514c8ac634827bf60179c
SampreetG22 <sampreet.g.45@gmail.com>
2024-10-09 09:19:30 +0530

    Changed Port

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require("cors");
 app.use(cors());
 app.use(express.json());
+const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
 app.listen(3000, () => {
   console.log(`Server is running on port ${port}`);
